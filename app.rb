09696d8a880474837fab5848bbe3159eab0f906f
gglin <george.g.lin@gmail.com>
2013-06-24 14:22:45 -0400

    Add student ORM

--- app.rb
+++ app.rb
@@ -1,10 +1,32 @@
 require 'sinatra/base'
+require_relative 'lib/models/student.rb'
 
 # Why is it a good idea to wrap our App class in a module?
 module StudentSite
   class App < Sinatra::Base
+
+    def initialize
+      super
+      @artists = ["Frank Sinatra", "Bing Crosby", "Bob\n\n\n\n", 1, 2, "dslajkffoiejfijasdlfl"]
+    end
+
+    def set_random_numbers(arg)
+      @random_numbers = arg
+    end
+
     get '/' do
       "hello world!"
     end
+
+    get '/hello-world' do
+      set_random_numbers((1..20).to_a)
+      p @random_numbers
+      erb :hello
+    end
+
+    get '/artists' do
+      erb :artists
+    end
+
   end
 end
---
+++ test.rb
@@ -0,0 +1,92 @@
+require 'fis/test'
+require 'sqlite3'
+require 'pry-debugger'
+
+require_relative 'lib/concerns/persistable'
+
+require_relative 'lib/models/student'
+
+include Fis::Test
+
+assert Student.drop
+assert Student.create_table
+
+test 'should create a table' do
+  assert !Student.table_exists?('students')
+
+  assert Student.table_exists?('students')
+end
+
+test 'should be able to instantiate a student' do
+  assert Student.new
+end
+
+test 'should be able to save a student with a name' do
+  s = Student.new
+  s.name = "Avi Flombaum"
+  s.save
+
+  assert_equal Student.find_by_name("Avi Flombaum").name, "Avi Flombaum"
+end
+
+test 'should be able to load all students' do
+  s = Student.new
+  s.name = "Avi Flombaum"
+  s.save
+
+  assert Student.all.collect{|s| s.name}.include?("Avi Flombaum")
+end
+
+test 'should be able to find a student by id' do
+  s = Student.new
+  s.name = "Avi Flombaum"
+  s.save
+
+  assert_equal Student.find(s.id).name, "Avi Flombaum"
+end
+
+test 'should be able to update a student' do
+  s = Student.new
+
+  s.name = "Avi Flombaum"
+  s.save
+
+  s.name = "Bob Whitney"
+  s.save
+
+  assert_equal Student.find(s.id).name, "Bob Whitney"
+end
+
+test 'should be able to retrive a student with a where statment' do
+  s = Student.new
+  s.name = "Jeff Baird"
+  s.save
+
+  assert_equal Student.where(:name => "Jeff Baird").map { |s| s.name }, [s.name]
+
+end
+
+test 'should be able to retrieve multiple students with the same name' do
+  s = Student.new
+  s.name = "Alice Adams"
+  s.save
+
+  s2 = Student.new
+  s2.name = "Alice Adams"
+  s2.save
+
+
+  assert_equal Student.where(:name => "Alice Adams").map {|s| s.name}, [s.name,s2.name]
+end
+
+test 'should be able to find_by any attribue' do
+  s = Student.new
+  s.name = "Avi Flombaum"
+  s.tagline = "Hello World"
+  s.bio = "Dean at Flatiron School"
+  s.save
+
+ assert_equal Student.find_by_tagline("Hello World").tagline, "Hello World"
+ assert_equal Student.find_by_bio("Dean at Flatiron School").bio, "Dean at Flatiron School"
+ assert_equal Student.find_by_name("Avi Flombaum").name, "Avi Flombaum"
+end
